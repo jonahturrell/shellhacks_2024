a1c0c72ff7f4e37795c85707e278b63967e63598
Jonah Turrell <jonahturrell@gmail.com>
2024-09-28 12:17:37 -0400

    Update

--- backendstuff/usagescripts/destroyuser.sh
+++ backendstuff/usagescripts/destroyuser.sh
@@ -1,7 +1,17 @@
-deluser $1
+#!/bin/bash
 
-echo ""
+date="$(date ; echo)"
+
+# Make sure a username has been input
+if [ -n "$1" ]; then 
+  username="$1"
+else
+  echo "$date - destroyuser.sh run without input" >> log.txt
+  exit 1
+fi
+
+userdel -r $1
 
 # Log
-date="$(date ; echo)"
 echo "$date - Deleted user $1" >> log.txt
+
