ab2ea64db126cbca13041b88fe0294a091983bfd
Jonah Turrell <jonahturrell@gmail.com>
2024-09-28 09:25:13 -0400

    Fix bug

--- backendstuff/usagescripts/createuser.sh
+++ backendstuff/usagescripts/createuser.sh
@@ -1,7 +1,8 @@
 username="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo)" # Get a random 13-character string
 useradd -m -G mail $username
 
-
 # Log
 date="$(date ; echo)"
 echo "Created user $username at $date" >> log.txt
+
+echo "$username"
