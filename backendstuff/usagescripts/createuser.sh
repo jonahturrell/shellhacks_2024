e1e03cd1d8ee04aae4636b2c3b872148c14e5a39
Jonah Turrell <jonahturrell@gmail.com>
2024-09-28 11:26:30 -0400

    Add custom username input

--- backendstuff/usagescripts/createuser.sh
+++ backendstuff/usagescripts/createuser.sh
@@ -1,8 +1,14 @@
-username="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo)" # Get a random 13-character string
+#!/bin/bash
+
+if [ -n "$1" ]; then
+  username="$1"
+else
+  username="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo)" # Get a random 13-character string
+
+
+fi
 useradd -m -G mail $username
 
 # Log
 date="$(date ; echo)"
 echo "Created user $username at $date" >> log.txt
-
-echo "$username"
