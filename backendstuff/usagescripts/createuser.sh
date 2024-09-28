4fa18e99da93db404aa45898152d6e3ceaa3b6ee
Jonah Turrell <jonahturrell@gmail.com>
2024-09-28 09:20:04 -0400

    Add logging

--- backendstuff/usagescripts/createuser.sh
+++ backendstuff/usagescripts/createuser.sh
@@ -2,5 +2,6 @@ username="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo)" # Get a random 
 useradd -m -G mail $username
 
 
+# Log
 date="$(date ; echo)"
 echo "Created user $username at $date" > log.txt
--- backendstuff/usagescripts/destroyuser.sh
+++ backendstuff/usagescripts/destroyuser.sh
@@ -1,1 +1,5 @@
 deluser $1
+
+# Log
+date="$(date ; echo)"
+echo "Deleted user $1 at $date" > log.txt
