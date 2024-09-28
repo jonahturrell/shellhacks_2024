4609309f37dbf1a001a81703f91bae2457fb40e0
Jonah Turrell <jonahturrell@gmail.com>
2024-09-28 16:53:30 -0400

    Quick fix

--- backendstuff/dosecond_setupserver.sh
+++ backendstuff/dosecond_setupserver.sh
@@ -1,8 +1,9 @@
+# Install Express
 apt install express -y
 
+# Set up the mail user control
 useradd -m dinoMailUserControl
-
 cp -r ./dinoMailUserControl/* /home/dinoMailUserControl
 echo "dinoMailUserControl ALL=(ALL) NOPASSWD: /usagescripts/createuser.sh, /usagescripts/destroyuser.sh" >> /etc/sudoers
 
-
+# Add server to systemctl
