3a552b93eba948213db9440baab54ad7076986ae
haleyabae22 <147110649+haleyabae22@users.noreply.github.com>
2024-09-29 11:15:27 -0400

    delete duplicat import statements

--- dino_mail/src/Login.jsx
+++ dino_mail/src/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import cute_dino from './assets/stegosaurus-transparent.png';
